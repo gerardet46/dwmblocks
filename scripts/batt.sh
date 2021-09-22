bdd403b082c9539a0c8cce4357ad534b242cf5fb
gerardet46 <gerard@ricebes.cat>
2021-09-22 09:50:40 +0200

    battery uploaded and cpu orange at 60%

---
+++ scripts/batt.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+colors() {
+    [ "$1" -lt "15" ] && echo "$COL_VERMELL" ||
+    [ "$1" -lt "35" ] && echo "$COL_TARONJA" ||
+    [ "$1" -lt "60" ] && echo "$COL_GROC"    ||
+    echo "$COL_BLANC"
+}
+
+icona() {
+    [ "$1" -lt "15" ] && echo "" ||
+    [ "$1" -lt "35" ] && echo "" ||
+    [ "$1" -lt "60" ] && echo "" ||
+    [ "$1" -lt "80" ] && echo "" ||
+    echo ""
+}
+
+IC_CURRENT="^c$COL_GROC^"
+IC_PLUG="^c$COL_BLANC^"
+
+acpi > /tmp/acpi_info
+while read line; do
+    percent="$(echo "$line" | awk -F ', ' '{print $2}' | sed 's/.$//')"
+
+    if echo "$line" | grep "Discharging," > /dev/null; then
+        # DESCARREGANT
+        printf "^c$(colors "$percent")^"
+        printf "$(icona "$percent") "
+        printf "$percent%%"
+
+        time_left="$(echo "$line" | awk '{print $5}' | sed "s/:[0-9][0-9]$//g")"
+        printf " ($time_left)"
+
+    elif echo "$line" | grep "Full" > /dev/null; then
+        # COMPLETAMENT CARREGADA
+        printf "$IC_PLUG 100%%"
+
+    elif ! echo "$line" | grep "Unknown" > /dev/null; then
+        # CARREGANT
+        printf "$IC_CURRENT $IC_PLUG $percent%%"
+
+    fi
+done < /tmp/acpi_info
