59947e7d7f9d3b899eea0ab36540839015709b51
gerardet46 <gerard@ricebes.cat>
2022-08-30 15:06:49 +0200

    mail status updated

--- blocks.h
+++ blocks.h
@@ -3,15 +3,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/             /*Update Interval*/ /*Update Signal*/
-    {"",	SC("spotify.sh"),	5,	            0 },
+    {"",	SC("spotify.sh"),	5,	            0  },
     {"",	SC("mpc.sh"),    	5,	            13 },
-    {"",	SC("checkupdates.sh"),	3600,               0 },
+    {"",	SC("checkupdates.sh"),	3600,               0  },
     {"",	SC("mail.sh"),      	300,                12 },
     {"",	SC("volume.sh"),	0,	            10 },
-    {"",	SC("batt.sh"),	        15,	            0 },
-    {"",	SC("cpu.sh"),           1,	            0 },
-    {"",	SC("memoria.sh"),	10,	            0 },
-    {"",	SC("date.sh"),	        15,	            0 },
+    {"",	SC("batt.sh"),	        15,	            0  },
+    {"",	SC("cpu.sh"),           1,	            0  },
+    {"",	SC("memoria.sh"),	10,	            0  },
+    {"",	SC("date.sh"),	        15,	            0  },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
